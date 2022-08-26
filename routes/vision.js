9578e0d2b3437114cc4093ebc85cd655bb9a22e9
Thiluxan <thiluxan@hotmail.co.uk>
2022-08-26 11:56:05 +0530

    Added Feature to Detect Labels in Image

--- routes/vision.js
+++ routes/vision.js
@@ -1,17 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var AWS = require('aws-sdk')
+require('dotenv').config()
 
 router.post('/classify', function(req, res, next) {
-  // DON'T return the hardcoded response after implementing the backend
-  let response = ["shoe", "red", "nike"];
 
   // Your code starts here //
+  AWS.config.update({
+    accessKeyId:process.env.ACCESS_KEY,
+    secretAccessKey:process.env.SECRET_KEY,
+    region:'ap-southeast-2'
+  })
+
+  var recoknition = new AWS.Rekognition()
+
+  var params = {
+    Image: {
+        Bytes:req.files.file.data
+    },
+    MaxLabels:20,
+    MinConfidence:80
+  };
+
+  recoknition.detectLabels(params,  (err, data) => {
+      if(err){
+          res.status(500).json({"error":"Unable to process the request"});
+      } else{
+          var response = [];
+          data.Labels.map(label => {response.push(label.Name)})
+          res.status(200).json({"labels":response})
+      }
+  })
 
   // Your code ends here //
 
-  res.json({
-    "labels": response
-  });
 });
 
 module.exports = router;
